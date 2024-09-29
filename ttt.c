00f103d75eb95993d0c45b19ac9e77751e4e91ba
sanjaymanohar <150341461+sanjaymanohar@users.noreply.github.com>
2024-09-29 17:07:36 +0530

    Update ttt.c

--- ttt.c
+++ ttt.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
 int main(){
-<<<<<<< HEAD
-printf("hi this me : sanjay");}
-=======
-printf("hi this me : sanjay")
-return 0;
-}
->>>>>>> 37495c3bf5a0751bee1ded631f744ea8299c1e62
+printf("hi this me : sanjay");
+
+return 0;}
